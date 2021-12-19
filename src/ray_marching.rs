60587cff0d1460f0b2ce7cf5ee3ebe1d6ae8a1eb
wrac4242 <wrac4242@gmail.com>
2021-12-19 22:06:07 +0000

    fix position to be between -1 and 1, not -0.5 and 0.5

--- src/ray_marching.rs
+++ src/ray_marching.rs
@@ -15,7 +15,8 @@ impl Renderer {
 	}
 
 	pub fn march_pixel(&mut self, pix: types::Point2) -> types::Colour {
-		let mut pix3 = types::Point3::new((pix.x/ self.width as f32) - 0.5, (pix.y / self.height as f32) -  0.5, 1.0);
+		// calculates the pixels xy position on the screen to be between -1 and 1
+		let mut pix3 = types::Point3::new(((2.0 * pix.x)/ self.width as f32) - 1.0, ((2.0 * pix.y) / self.height as f32) -  1.0, 1.0); 
 		pix3.normalize();
 		pix3 = pix3.mul(255.0/2.0);
 		[(pix3.x + 255.0/2.0) as u8, (pix3.y + 255.0/2.0) as u8, 0x00, 0xff]
